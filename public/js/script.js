8a3a091b37a1eea2c942b5eb755d520c8e673427
Kyle Butler <kylbutlr@gmail.com>
2018-11-16 23:23:58 -0800

    some basic css/jq

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
-/* jshint asi: true */
-/* jshint esversion: 6 */
-
+$(function(){
+  $("h6").hide().delay(3000).fadeToggle(1000)
+  $(".footer-div").hide().delay(500).slideToggle(1000)
+  $(".navigation-div").hide().delay(500).slideToggle(1000)
+  $(".header-div").hide().delay(2000).fadeToggle(1000)
+  $(".socials-div").hide().delay(2000).fadeToggle(1000)
+})
