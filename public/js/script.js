d8450cdaaf87b0aff818dda96b7ea210b05cd564
Kyle Butler <kylbutlr@gmail.com>
2019-01-12 14:43:45 -0800

    minor change

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,34 @@
 $(function(){
-  $(".projects-window").hide()
-  $(".background").hide().delay(1000).fadeToggle(3000)
-  $(".header-div").hide().delay(500).fadeToggle(1500)
-  $(".socials-div").hide().delay(500).fadeToggle(1500)
+  $(".projects-window").hide();
+  $(".background").hide().delay(1000).fadeToggle(3000);
+  $(".header-div").hide().delay(500).fadeToggle(1500);
+  $(".socials-div").hide().delay(500).fadeToggle(1500);
   setTimeout(function() {
-    $(".footer-button").trigger('click')
-  }, 3000)
+    $(".footer-button").trigger('click');
+  }, 3000);
 
   $(".footer-button").on("click", function(){
     if ($(".projects-window").is(":hidden")){
-      $(".menu-bars").addClass("full-opacity")
-      $(".projects-window").stop().delay(500).fadeIn(500)
-      $(".footer-div").stop().animate({bottom: 12},750)
+      $(".menu-bars").addClass("full-opacity");
+      $(".projects-window").stop().delay(500).fadeIn(500);
+      $(".footer-div").stop().animate({bottom: 12},750);
     } else {
-      $(".menu-bars").removeClass("full-opacity")
-      $(".projects-window").fadeOut(500)
-      $(".footer-div").stop().animate({bottom: -110},750)
+      $(".menu-bars").removeClass("full-opacity");
+      $(".projects-window").fadeOut(500);
+      $(".footer-div").stop().animate({bottom: -110},750);
     }
-  })
+  });
   
   function scrollHorizontally(e) {
       e = window.event || e;
-      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)))
-      document.getElementById('scrollDiv').scrollLeft -= (delta*120) // (*40)
-      e.preventDefault()
+      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      document.getElementById('scrollDiv').scrollLeft -= (delta*120); // (*40)
+      e.preventDefault();
   }
   if (document.getElementById('scrollDiv').addEventListener) {
-      document.getElementById('scrollDiv').addEventListener("mousewheel", scrollHorizontally, false) // IE9, Chrome, Safari, Opera
-      document.getElementById('scrollDiv').addEventListener("DOMMouseScroll", scrollHorizontally, false) // Firefox
+      document.getElementById('scrollDiv').addEventListener("mousewheel", scrollHorizontally, false); // IE9, Chrome, Safari, Opera
+      document.getElementById('scrollDiv').addEventListener("DOMMouseScroll", scrollHorizontally, false); // Firefox
   } else {
-      document.getElementById('scrollDiv').attachEvent("onmousewheel", scrollHorizontally) // IE 6/7/8
+      document.getElementById('scrollDiv').attachEvent("onmousewheel", scrollHorizontally); // IE 6/7/8
   }
-})
+});
