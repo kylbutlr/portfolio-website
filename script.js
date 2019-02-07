415ba1861b7f7cc08ff070eb521b5d764fc77cd2
Kyle Butler <kylbutlr@gmail.com>
2019-02-06 18:12:33 -0800

    Revert "mouseleave fix for 'copied' notification"
    This reverts commit bb1cc663b1405ccb1c94680d0307bb467a1b7063.

--- script.js
+++ script.js
@@ -22,22 +22,15 @@ $(function() {
   $('.email').on('click', function() {
     let $temp = $('<input>');
     $('body').append($temp);
-    $temp.val($('.email').text()).select();
+    $temp.val($('#email').text()).select();
     document.execCommand('copy');
     $temp.remove();
     if ($('.copied').hasClass('full-opacity') === false) {
-      let tempTimeout;
       $('.copied').addClass('full-opacity');
-      $('.socials-div').mouseleave(function() {
-        $('.copied').removeClass('full-opacity');
-        $('.socials-div').off("mouseleave");
-        clearTimeout(tempTimeout);
-      });
-      tempTimeout = setTimeout(function() {
-        $('.copied').removeClass('full-opacity');
-        $('.socials-div').off("mouseleave");
-      }, 4000);
     }
+    setTimeout(function() {
+      $('.copied').removeClass('full-opacity');
+    }, 4000);
   });
 
   $('.footer-button').on('click', function() {
