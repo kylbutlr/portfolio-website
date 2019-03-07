70d172e9208fd832df130ead69d00d2a70a644d7
Kyle Butler <kylbutlr@gmail.com>
2019-03-06 20:58:07 -0800

    redesign WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link, Element, scroller } from "react-scroll";
 
 const tabs = {
   HOME: 1,
@@ -8,12 +9,17 @@ const tabs = {
 class App extends Component {
   constructor(props) {
     super(props);
-    //this.function = this.function.bind(this);
     this.state = {
       activeTab: tabs.HOME,
     };
   }
-
+  scrollTo() {
+    scroller.scrollTo('scroll-to-element', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
   render() {
     return (
       <div className='App'>
@@ -25,7 +31,7 @@ class App extends Component {
             <h3>kylbutlr</h3>
           </div>
           <div className='contact-link'>
-            <h4>contact</h4>
+            <Link activeClass="active" to="contact-element" spy={true} smooth={true} duration={500} ><h4>contact</h4></Link>
           </div>
         </div>
 
@@ -33,6 +39,7 @@ class App extends Component {
         <div className='Body'>
           <div className='divider' />
           <div className='welcome'>
+            <div className='headshot' />
             <h6>
               I'm <span className='bold'>Kyle Butler</span>, a junior web developer currently
               working in Los Angeles, CA.
@@ -42,12 +49,14 @@ class App extends Component {
           <div className='bio'>
             <p>
               I specialize in <span className='bold'>React</span>,{' '}
-              <span className='bold'>JavaScript</span>, and <span className='bold'>CSS + HTML</span>.
+              <span className='bold'>JavaScript</span>, and <span className='bold'>CSS + HTML</span>
+              .
             </p>
             <p>
               I have also had past experience working with <span className='bold'>SQL</span>,{' '}
               <span className='bold'>Java</span>, <span className='bold'>Python</span>, and{' '}
-              <span className='bold'>PHP</span>, and look forward to learning and utilizing many more languages/front-ends.
+              <span className='bold'>PHP</span>, and look forward to learning and utilizing many
+              more languages and front-ends throughout my career.
             </p>
           </div>
           <div className='divider' />
@@ -74,16 +83,49 @@ class App extends Component {
                 </div>
               </a>
             </div>
-
-            {/*<a href='https://github.com/kylbutlr/notes-app-api' />
-            <a href='https://github.com/kylbutlr/todo-list' />
-            <a href='https://github.com/kylbutlr/alarm-timer' />
-            <a href='https://github.com/kylbutlr/flex-slideshow' />
-            <a href='https://github.com/kylbutlr/tip-recorder' />*/}
+            <div className='project4'>
+              <a href='https://github.com/kylbutlr/alarm-timer'>
+                <div className='project-image'>
+                  <h6>Alarm & Timer</h6>
+                </div>
+              </a>
+            </div>
+            <div className='project5'>
+              <a href='https://github.com/kylbutlr/flex-slideshow'>
+                <div className='project-image'>
+                  <h6>Flex Slideshow</h6>
+                </div>
+              </a>
+            </div>
+            <div className='project6'>
+              <a href='https://github.com/kylbutlr/tip-recorder'>
+                <div className='project-image'>
+                  <h6>Tip Recorder</h6>
+                </div>
+              </a>
+            </div>
           </div>
           <div className='divider' />
           <div className='contact'>
-            <h5>CONTACT</h5>
+            <Element name='contact-element' className='element'>
+            <h5>CONTACT</h5></Element>
+            <p>
+              Feel free to reach out at: <a href='mailto:kylbutlr@gmail.com'>kylbutlr@gmail.com</a>
+            </p>
+            <div className='socials'>
+              <a href='mailto:kylbutlr@gmail.com'>
+                <div className='social-email' />
+              </a>
+              <a href='https://github.com/kylbutlr'>
+                <div className='social-github' />
+              </a>
+              <a href='https://www.linkedin.com/in/kylbutlr/'>
+                <div className='social-linkedin' />
+              </a>
+              <a href='https://www.instagram.com/kylbutlr/'>
+                <div className='social-instagram' />
+              </a>
+            </div>
           </div>
           <div className='divider last-divider' />
           <div className='footer'>
