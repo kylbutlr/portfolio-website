6a987eaaf76dc0ccb1816256756eb99f41fae622
Kyle Butler <kylbutlr@gmail.com>
2019-03-07 18:19:24 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# React Portfolio
-### My portfolio website hosted on Github Pages
-### Preview: [kylbutlr.github.io](https://kylbutlr.github.io/)
-
-![alt text](https://i.imgur.com/29jrFP1.png "Preview of kylbutlr.github.io")
---
+++ precache-manifest.8b992bd318afa1275789743b83d3104a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "7485b8b59549d69669d242898ae93a6c",
+    "url": "/static/media/alarm-timer_100.7485b8b5.png"
+  },
+  {
+    "revision": "c3a58d252c2305d1b376",
+    "url": "/static/css/main.a2e02066.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e42e5efc6b518780f7e2",
+    "url": "/static/js/2.e42e5efc.chunk.js"
+  },
+  {
+    "revision": "1c74b6c8956108e9ee42ea7d88c01385",
+    "url": "/static/media/notes-react_75.1c74b6c8.png"
+  },
+  {
+    "revision": "3ca16753ff015cb4432c5becfc4e3e8d",
+    "url": "/static/media/notes-react_100.3ca16753.png"
+  },
+  {
+    "revision": "bd99f124d37cfd9caecec4fe24094334",
+    "url": "/static/media/FuturaPTMedium.bd99f124.otf"
+  },
+  {
+    "revision": "9ffa6bf202106b4d9193776781f7a898",
+    "url": "/static/media/notes-api_100.9ffa6bf2.png"
+  },
+  {
+    "revision": "db10a46c3592d99556c22c455d28de10",
+    "url": "/static/media/notes-api_75.db10a46c.png"
+  },
+  {
+    "revision": "c3a58d252c2305d1b376",
+    "url": "/static/js/main.c3a58d25.chunk.js"
+  },
+  {
+    "revision": "156517877275e5368617afb5330fde03",
+    "url": "/static/media/alarm-timer_75.15651787.png"
+  },
+  {
+    "revision": "7ac6cac9155c71e933581fef6a283c8a",
+    "url": "/static/media/tip-recorder_100.7ac6cac9.png"
+  },
+  {
+    "revision": "87e1e10bf982107ec4c68998c8ac34eb",
+    "url": "/static/media/tip-recorder_75.87e1e10b.png"
+  },
+  {
+    "revision": "2ebeb6bffe29efa55b733c49c5e0592e",
+    "url": "/static/media/todo-list_75.2ebeb6bf.png"
+  },
+  {
+    "revision": "8313604cdb763a0ee7cdc664ac89ab75",
+    "url": "/static/media/todo-list_100.8313604c.png"
+  },
+  {
+    "revision": "475304d813d9b8856d4de7324e287cbf",
+    "url": "/static/media/headshot.475304d8.png"
+  },
+  {
+    "revision": "f258cbca8cfcaed8aa583c8a8a4a9813",
+    "url": "/static/media/flex-slideshow_100.f258cbca.png"
+  },
+  {
+    "revision": "0dc24d98b560ff81c85925b48997b4c6",
+    "url": "/static/media/flex-slideshow_75.0dc24d98.png"
+  },
+  {
+    "revision": "755e47a126861de80fbc44a21a722815",
+    "url": "/index.html"
+  }
+];
