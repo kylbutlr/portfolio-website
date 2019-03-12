70ade2cf458b4fd1f17d309258fa067b1f5c41b2
Kyle Butler <kylbutlr@gmail.com>
2019-03-12 16:59:38 -0700

    bio update

--- src/Biography.js
+++ src/Biography.js
@@ -4,14 +4,18 @@ export default () => (
   <div className='bio'>
     <p>
       I specialize in using <span className='bold'>React</span>,{' '}
-      <span className='bold'>JavaScript</span>, and <span className='bold'>CSS/HTML</span> to build
-      anything from responsive small business websites to rich interactive web applications.
+      <span className='bold'>JavaScript (ES6)</span>, <span className='bold'>CSS/HTML</span>, and{' '}
+      <span className='bold'>Node</span> to build anything from responsive small business or
+      personal websites to rich interactive web applications.
     </p>
     <p>
-      I also have experience working with <span className='bold'>Git</span>,{' '}
-      <span className='bold'>SQL</span>, <span className='bold'>Java</span>,{' '}
-      <span className='bold'>Python</span>, <span className='bold'>PHP</span>, and look forward to
-      learning and utilizing many more languages and frameworks throughout my career.
+      I have experience working with <span className='bold'>PostgreSQL</span>,{' '}
+      <span className='bold'>Express</span>, <span className='bold'>Axios</span>,{' '}
+      <span className='bold'>NPM</span>, <span className='bold'>Jest</span>,{' '}
+      <span className='bold'>Sass</span>, and <span className='bold'>Bulma</span>, as well as
+      had previously used <span className='bold'>Java</span>, <span className='bold'>PHP</span>,
+      and <span className='bold'>Python</span>. I look forward to learning and utilizing many more
+      languages and frameworks throughout my career.
     </p>
   </div>
 );
