55a63313f7b3f6392e00c917fc9fc2be7b15cf72
Kyle Butler <kylbutlr@gmail.com>
2019-03-12 17:19:26 -0700

    minor change

--- src/Biography.js
+++ src/Biography.js
@@ -5,7 +5,7 @@ export default () => (
     <p>
       I specialize in using <span className='bold'>React</span>,{' '}
       <span className='bold'>JavaScript (ES6)</span>, <span className='bold'>CSS/HTML</span>, and{' '}
-      <span className='bold'>Node</span> to build anything from responsive small business or
+      <span className='bold'>Node</span> to build anything from responsive small business and
       personal websites to rich interactive web applications.
     </p>
     <p>
@@ -13,8 +13,8 @@ export default () => (
       <span className='bold'>Express</span>, <span className='bold'>Axios</span>,{' '}
       <span className='bold'>NPM</span>, <span className='bold'>Jest</span>,{' '}
       <span className='bold'>Sass</span>, and <span className='bold'>Bulma</span>, as well as
-      had previously used <span className='bold'>Java</span>, <span className='bold'>PHP</span>,
-      and <span className='bold'>Python</span>. I look forward to learning and utilizing many more
+      previously used <span className='bold'>Java</span>, <span className='bold'>PHP</span>, and{' '}
+      <span className='bold'>Python</span>. I look forward to learning and utilizing many more
       languages and frameworks throughout my career.
     </p>
   </div>
