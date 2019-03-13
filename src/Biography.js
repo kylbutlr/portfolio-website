b07cc99898954f979abac57eec91333fc180205d
Kyle Butler <kylbutlr@gmail.com>
2019-03-13 16:31:06 -0700

    minor changes

--- src/Biography.js
+++ src/Biography.js
@@ -4,14 +4,13 @@ export default () => (
   <div className='bio'>
     <p>
       I specialize in using <span className='bold'>React</span>,{' '}
-      <span className='bold'>JavaScript&nbsp;(ES6)</span>, <span className='bold'>CSS/HTML</span>, and{' '}
-      <span className='bold'>Node</span> to&nbsp;build&nbsp;anything from responsive small&nbsp;business&nbsp;and
+      <span className='bold'>JavaScript</span>, <span className='bold'>Node</span>, and <span className='bold'>CSS&nbsp;+&nbsp;HTML</span> to&nbsp;build&nbsp;anything from responsive small&nbsp;business&nbsp;and
       personal websites to rich&nbsp;interactive&nbsp;web&nbsp;applications.
     </p>
     <p>
       I have experience working with <span className='bold'>PostgreSQL</span>,{' '}
       <span className='bold'>Express</span>, <span className='bold'>Axios</span>,{' '}
-      <span className='bold'>Jest</span>, <span className='bold'>Sass</span>, and{' '}
+      <span className='bold'>Jest</span>, <span className='bold'>Babel</span>, <span className='bold'>Sass</span>, and{' '}
       <span className='bold'>Bulma</span>, as well as previously used{' '}
       <span className='bold'>Java</span>, <span className='bold'>PHP</span>, and{' '}
       <span className='bold'>Python</span>. Web Development is constantly evolving and I look
--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ export default () => (
     <div className='headshot' />
     <h6>
       I'm&nbsp;<span className='bold'>Kyle&nbsp;Butler</span>,
-      <br />a&nbsp;junior&nbsp;web&nbsp;developer
+      <br />a&nbsp;junior&nbsp;front&nbsp;end&nbsp;web&nbsp;developer
       currently <span className='bold'>looking&nbsp;for&nbsp;work</span>&nbsp;in&nbsp;Los&nbsp;Angeles,&nbsp;CA.
     </h6>
   </div>
