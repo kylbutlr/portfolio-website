37253e880595937ad9c3b0989e3cce1088bf7d38
Kyle Butler <kylbutlr@gmail.com>
2019-03-19 11:26:57 -0700

    minor changes

--- src/Biography.js
+++ src/Biography.js
@@ -3,18 +3,24 @@ import React from 'react';
 export default () => (
   <div className='bio'>
     <p>
-      I specialize in using <span className='bold'>React</span>,{' '}
-      <span className='bold'>JavaScript</span>, <span className='bold'>Node</span>, and <span className='bold'>CSS&nbsp;+&nbsp;HTML</span> to&nbsp;build&nbsp;anything from responsive small&nbsp;business&nbsp;and
-      personal websites to rich&nbsp;interactive&nbsp;web&nbsp;applications.
+      I specialize in using <span className='bold'>JavaScript</span>,{' '}
+      <span className='bold'>React</span>, <span className='bold'>Redux</span>,{' '}
+      <span className='bold'>Node</span>, and <span className='bold'>CSS&nbsp;+&nbsp;HTML</span>{' '}
+      to build anything from responsive small business and personal websites to
+      rich interactive web&nbsp;applications.
     </p>
     <p>
       I have experience working with <span className='bold'>PostgreSQL</span>,{' '}
       <span className='bold'>Express</span>, <span className='bold'>Axios</span>,{' '}
-      <span className='bold'>Jest</span>, <span className='bold'>Babel</span>, <span className='bold'>Sass</span>, and{' '}
-      <span className='bold'>Bulma</span>, as well as previously used{' '}
-      <span className='bold'>Java</span>, <span className='bold'>PHP</span>, and{' '}
-      <span className='bold'>Python</span>. Web Development is constantly evolving and I look
-      forward to learning and utilizing many more tools, languages, and frameworks throughout&nbsp;my&nbsp;career.
+      <span className='bold'>Jest</span>, <span className='bold'>Babel</span>,{' '}
+      <span className='bold'>Sass</span>, <span className='bold'>Bulma</span>, <span className='bold'>Heroku</span>, and{' '}
+      <span className='bold'>Git</span>, as well as previously used{' '}
+      <span className='bold'>jQuery</span>, <span className='bold'>Java</span>, <span className='bold'>PHP</span>,&nbsp;and&nbsp;
+      <span className='bold'>Python</span>.
+    </p>
+    <p>
+      Web Development is rapidly evolving and I look forward to learning and utilizing many more
+      tools, languages, and frameworks throughout&nbsp;my&nbsp;career.
     </p>
   </div>
 );
