5daa5980adf287acad5e3fcda51fca544d65b8d3
Kyle Butler <kylbutlr@gmail.com>
2019-03-24 18:10:27 -0700

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,26 @@
-plugins: [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `data`,
-      path: `${__dirname}/src/data/`
-    }
+module.exports = {
+  siteMetadata: {
+    title: `Kyle Butler`,
+    siteUrl: `https://kylbutlr.com`,
+    description: `Kyle Butler's Portfolio`,
   },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `img`,
-      path: `${__dirname}/src/images/`
-    }
-  },
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`
-]
+
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+};
