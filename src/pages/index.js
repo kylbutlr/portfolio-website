fd4526f5ae01fd665056ad5274baab2e300bd2a4
Kyle Butler <kylbutlr@gmail.com>
2019-03-27 18:16:12 -0700

    custom scrollbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import '../App.css';
 import { scroller } from 'react-scroll';
+import { Scrollbars } from 'react-custom-scrollbars';
 import Header from '../components/Header';
 import Welcome from '../components/Welcome';
 import Biography from '../components/Biography';
@@ -10,6 +11,19 @@ import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
 class App extends Component {
+  componentDidMount() {
+    this.runResize();
+  }
+  
+  runResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+  
   scrollTo() {
     scroller.scrollTo('scroll-to-element', {
       duration: 800,
@@ -17,38 +31,40 @@ class App extends Component {
       smooth: 'easeInOutQuart',
     });
   }
-  
+
   render() {
     return (
-      <div className='App'>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Kyle Butler</title>
-          <link rel="canonical" href="https://kylbutlr.com" />
-        </Helmet>
-        <Header />
-        <div className='divider first-divider' />
-        <div className='Body'>
-          <div className='main-app'>
-            <Welcome />
-            <div className='divider' />
-            <Biography />
-            <div className='divider' />
-            <div className='projects-underneath'>
-              <Work />
+      <Scrollbars autoHide autoHeight autoHeightMax={`calc(var(--vh, 1vh) * 100)`}>
+        <div className='App'>
+          <Helmet>
+            <meta charSet='utf-8' />
+            <title>Kyle Butler</title>
+            <link rel='canonical' href='https://kylbutlr.com' />
+          </Helmet>
+          <Header />
+          <div className='divider first-divider' />
+          <div className='Body'>
+            <div className='main-app'>
+              <Welcome />
               <div className='divider' />
+              <Biography />
+              <div className='divider' />
+              <div className='projects-underneath'>
+                <Work />
+                <div className='divider' />
+              </div>
+              <Contact />
             </div>
-            <Contact />
+            <div className='side-app'>
+              <Work />
+            </div>
           </div>
-          <div className='side-app'>
-            <Work />
+          <div className='small-device'>
+            <h6>Sorry, this device's screen is too small to view this website properly.</h6>
           </div>
+          <Footer />
         </div>
-        <div className='small-device'>
-          <h6>Sorry, this device's screen is too small to view this website properly.</h6>
-        </div>
-        <Footer />
-      </div>
+      </Scrollbars>
     );
   }
 }
