b2f881084415728d600218ff17ad0e77218ae2ad
Kyle Butler <kylbutlr@gmail.com>
2019-03-27 19:55:24 -0700

    favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,13 @@ class App extends Component {
           <meta charSet='utf-8' />
           <title>Kyle Butler</title>
           <link rel='canonical' href='https://kylbutlr.com' />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e3ccc4"/>
+          <meta name="msapplication-TileColor" content="#111111"/>
+          <meta name="theme-color" content="#111111"/>
         </Helmet>
         <Header />
         <div className='divider first-divider' />
