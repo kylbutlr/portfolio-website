0567c3b63fcbaaf372c9b215f16615476fb657e7
Kyle Butler <kylbutlr@gmail.com>
2019-03-28 17:17:20 -0700

    minor changes

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -9,26 +9,29 @@ export default () => (
       small business&nbsp;and&nbsp;personal websites to rich interactive&nbsp;web&nbsp;applications.
     </p>
     <p>
-      I have experience working with a variety of tools and concepts such as{' '}
-      <span className='bold'>Redux</span>, <span className='bold'>SQL</span>,{' '}
-      <span className='bold'>Postgres</span>, <span className='bold'>RESTful&nbsp;APIs</span>,{' '}
-      <span className='bold'>Express</span>, <span className='bold'>Axios</span>, Unit & Integration
-      Testing (<span className='bold'>Jest</span>), Compilers (<span className='bold'>Babel</span>),
-      Preprocessors (<span className='bold'>Sass</span>), CSS Frameworks (
-      <span className='bold'>Bulma</span>,&nbsp;
-      <span className='bold'>Bootstrap</span>), and Version Control Systems (
-      <span className='bold'>Git</span>,&nbsp;<span className='bold'>Github</span>).
-      I&nbsp;am&nbsp;also previously familiar with <span className='bold'>jQuery</span>,{' '}
-      <span className='bold'>Java</span>,&nbsp;<span className='bold'>PHP</span>,&nbsp;and&nbsp;
-      <span className='bold'>Python</span>.
+      I have experience working with a variety of tools and concepts such as:{' '}
+      <span className='bold'>RESTful APIs</span>, <span className='bold'>SQL</span>,{' '}
+      <span className='bold'>Postgres</span>, <span className='bold'>Express</span>,{' '}
+      <span className='bold'>Axios</span>, <span className='bold'>Redux</span>, Unit & Integration
+      Testing (<span className='bold'>Jest</span>), Compilers & Preprocessors (
+      <span className='bold'>Babel</span>, <span className='bold'>Sass</span>), CSS Frameworks (
+      <span className='bold'>Bulma</span>, <span className='bold'>Bootstrap</span>), and Version
+      Control Systems (<span className='bold'>Git</span>, <span className='bold'>Github</span>
+      ). I&nbsp;am&nbsp;also previously familiar with: <span className='bold'>jQuery</span>,{' '}
+      <span className='bold'>Java</span>,&nbsp;<span className='bold'>Python</span>,&nbsp;and&nbsp;
+      <span className='bold'>PHP</span>.
     </p>
-    <p> {/*style={{ display: 'none' }}>*/}
+    <p>
+      {' '}
+      {/*style={{ display: 'none' }}>*/}
       Web Development is rapidly evolving and I look forward to learning and utilizing many more
       frameworks, libraries, and languages throughout&nbsp;my&nbsp;career.
     </p>
-    <p> {/*style={{ display: 'none' }}>*/}
-      I am hoping to join an innovative company that is looking for a junior developer that is eager to
-      learn and become a valuable asset&nbsp;to&nbsp;the&nbsp;team.
+    <p>
+      {' '}
+      {/*style={{ display: 'none' }}>*/}I am hoping to join an innovative company that is looking
+      for a junior developer who is eager to learn and become a valuable
+      asset&nbsp;to&nbsp;the&nbsp;team.
     </p>
   </div>
 );
