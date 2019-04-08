70d61eba1df07db8ddb0a7137244d4f4e184741f
Kyle Butler <kylbutlr@gmail.com>
2019-04-08 13:36:32 -0700

    minor change

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -12,13 +12,14 @@ export default () => (
       I have experience working with a variety of tools and concepts such as:{' '}
       <span className='bold'>RESTful APIs</span>, <span className='bold'>SQL</span>,{' '}
       <span className='bold'>Postgres</span>, <span className='bold'>Express</span>,{' '}
-      <span className='bold'>Axios</span>, <span className='bold'>Redux</span>, Unit & Integration
-      Testing (<span className='bold'>Jest</span>), Compilers & Preprocessors (
+      <span className='bold'>Redux</span>, Unit & Integration Testing (
+      <span className='bold'>Jest</span>), Compilers & Preprocessors (
       <span className='bold'>Babel</span>, <span className='bold'>Sass</span>), CSS Frameworks (
-      <span className='bold'>Bulma</span>, <span className='bold'>Bootstrap</span>), and Version
-      Control Systems (<span className='bold'>Github</span>, <span className='bold'>Git</span>
-      ). I&nbsp;am&nbsp;also previously familiar with: <span className='bold'>jQuery</span>,{' '}
-      <span className='bold'>Java</span>,&nbsp;<span className='bold'>Python</span>,&nbsp;and&nbsp;
+      <span className='bold'>Bulma</span>, <span className='bold'>Bootstrap</span>), Version Control
+      Systems (<span className='bold'>Github</span>, <span className='bold'>Git</span>
+      ), and more. I&nbsp;am&nbsp;also previously familiar with:{' '}
+      <span className='bold'>jQuery</span>, <span className='bold'>Java</span>,&nbsp;
+      <span className='bold'>Python</span>,&nbsp;and&nbsp;
       <span className='bold'>PHP</span>.
     </p>
     <p>
@@ -26,8 +27,8 @@ export default () => (
       frameworks, libraries, and languages throughout&nbsp;my&nbsp;career.
     </p>
     <p>
-      I am hoping to join an innovative organization that is looking for a web developer who is eager
-      to learn and become a valuable asset&nbsp;to&nbsp;the&nbsp;team!
+      I am hoping to join an innovative organization that is looking for a web developer who is
+      eager to learn and become a valuable asset&nbsp;to&nbsp;the&nbsp;team!
     </p>
   </div>
 );
