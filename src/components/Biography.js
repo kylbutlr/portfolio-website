440bfdf01e0a494521541ff200fe2492fcbf17d6
Kyle Butler <kylbutlr@gmail.com>
2019-04-28 16:25:02 -0700

    4/2019 update

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -4,22 +4,21 @@ export default function Biography() {
   return (
     <div className='bio'>
       <p>
-        I specialize in using <strong>Vue</strong>, <strong>React</strong>
-        , <strong>Node</strong>, <strong>JavaScript</strong>,{' '}
-        <strong>PHP</strong>, and&nbsp;<strong>CSS</strong>
+        I use <strong>Vue</strong>, <strong>React</strong>, <strong>Node</strong>,{' '}
+        <strong>PHP</strong>, <strong>JavaScript</strong>, and <strong>CSS</strong>
         <span className='slant'>/</span>
-        <strong>HTML</strong> to build anything from small business websites to
-        intuitive progressive web&nbsp;applications.
+        <strong>HTML</strong> to build anything from small business websites to intuitive
+        progressive web&nbsp;applications.
       </p>
       <p>
         I have experience working with a variety of tools and concepts such as:{' '}
-        <strong>RESTful APIs</strong>, <strong>SQL</strong>,{' '}
-        <strong>Postgres</strong>, <strong>Express</strong>,{' '}
-        <strong>Redux</strong>, Unit & Integration Testing (
-        <strong>Jest</strong>), Compilers & Preprocessors (
-        <strong>Babel</strong>, <strong>Sass</strong>), CSS Frameworks (
-        <strong>Bootstrap</strong>, <strong>Bulma</strong>), Version
-        Control Systems (<strong>Git</strong>, <strong>Github</strong>
+        <strong>RESTful APIs</strong>, <strong>SQL</strong>
+        <span className='slant'>/</span>
+        <strong>Postgres</strong>, <strong>Express</strong>, <strong>Redux</strong>, Unit &
+        Integration Testing (<strong>Jest</strong>), Compilers & Preprocessors (
+        <strong>Babel</strong>, <strong>Sass</strong>), CSS Frameworks (<strong>Bootstrap</strong>,{' '}
+        <strong>Bulma</strong>), Version Control Systems (<strong>Git</strong>,{' '}
+        <strong>Github</strong>
         ), and more. I am also previously familiar with: <strong>jQuery</strong>,{' '}
         <strong>Java</strong>
         ,&nbsp;and&nbsp;<strong>Python</strong>.
@@ -30,7 +29,7 @@ export default function Biography() {
       </p>
       <p style={{ display: 'none' }}>
         I am hoping to join a growing organization that is looking for an innovative web developer
-        who is eager to learn, adapt, and become a valuable asset to&nbsp;the&nbsp;whole&nbsp;team!
+        who is eager to learn, adapt, and become a valuable asset to the&nbsp;whole&nbsp;team!
       </p>
     </div>
   );
