f70c593d4a860123e2072277f3104d1d75924003
Kyle Butler <kylbutlr@gmail.com>
2019-04-27 18:16:59 -0700

    4/2019 update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,30 +6,31 @@ import Header from '../components/Header';
 import Body from '../components/Body';
 import Footer from '../components/Footer';
 
+const windowGlobal = typeof window !== 'undefined' && window;
+const documentGlobal = typeof document !== 'undefined' && document;
+
 class App extends Component {
   componentDidMount() {
-    const windowGlobal = typeof window !== 'undefined' && window;
-    const documentGlobal = typeof document !== 'undefined' && document;
     this.runResize(documentGlobal, windowGlobal);
     this.fadeIn(documentGlobal, windowGlobal);
   }
   
-  runResize(document, window) {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+  runResize(documentGlobal, windowGlobal) {
+    let vh = windowGlobal.innerHeight * 0.01;
+    documentGlobal.documentElement.style.setProperty('--vh', `${vh}px`);
+    windowGlobal.addEventListener('resize', () => {
+      let vh = windowGlobal.innerHeight * 0.01;
+      documentGlobal.documentElement.style.setProperty('--vh', `${vh}px`);
     });
   }
 
-  fadeIn(document, window) {
-    document.getElementById('App').classList.add('fade-in');
-    document.getElementById('background').classList.add('fade-in');
-    window.setTimeout(() => {
-      document.getElementById('mainApp').classList.add('fade-in');
-      window.setTimeout(() => {
-        document.getElementById('sideApp').classList.add('fade-in');
+  fadeIn(documentGlobal, windowGlobal) {
+    documentGlobal.getElementById('App').classList.add('fade-in');
+    documentGlobal.getElementById('background').classList.add('fade-in');
+    windowGlobal.setTimeout(() => {
+      documentGlobal.getElementById('mainApp').classList.add('fade-in');
+      windowGlobal.setTimeout(() => {
+        documentGlobal.getElementById('sideApp').classList.add('fade-in');
       }, 1000);
     }, 1000);
   }
