432a211a129ec8846f09c7f6fe27865b6dc505cf
Kyle Butler <kylbutlr@gmail.com>
2019-08-09 16:07:21 -0700

    minor change

--- src/components/readType.js
+++ src/components/readType.js
@@ -1,8 +1,12 @@
 export default function readType(type) {
   let readType;
-  if (type === 'react' || type === 'node') {
+
+  //Adds ".js" to javascript languages as desired
+  /*if (type === 'react' || type === 'node' || type === 'vue') {
     type = type + '.js';
-  }
+  }*/
+
+  //Capitalizes the S in JavaScript
   if (type === 'javascript') {
     readType =
       type.substring(0, 1).toUpperCase() +
@@ -10,8 +14,10 @@ export default function readType(type) {
       type.substring(4, 5).toUpperCase() +
       type.substring(5);
   } else if (type === 'css') {
-    readType = type.toUpperCase() + '3';
+    //Capitalizes whole word
+    readType = type.toUpperCase();
   } else {
+    //Capitalizes first character
     readType = type.substring(0, 1).toUpperCase() + type.substring(1);
   }
   return readType;
