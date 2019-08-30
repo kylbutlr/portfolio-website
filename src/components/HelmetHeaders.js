e27b95e8018052b95572ca529fa61b241e0fea9a
Kyle Butler <kylbutlr@gmail.com>
2019-08-30 15:51:32 -0700

    minor change

--- src/components/HelmetHeaders.js
+++ src/components/HelmetHeaders.js
@@ -15,9 +15,9 @@ export default function HelmetHeaders() {
       <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
       <link rel='manifest' href='/site.webmanifest' />
-      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#dcbeb3' />
-      <meta name='msapplication-TileColor' content='#dcbeb3' />
-      <meta name='theme-color' content='#dcbeb3' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#222222' />
+      <meta name='msapplication-TileColor' content='#222222' />
+      <meta name='theme-color' content='#222222' />
       <meta name="Description" content="Kyle Butler's Portfolio Website" />
     </Helmet>
   );
