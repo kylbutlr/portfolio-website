363761245911f2568600668c274d12381268368d
Kyle Butler <kylbutlr@gmail.com>
2019-08-30 11:45:24 -0700

    minor change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome() {
   return (
     <div className='welcome'>
       <div className='headshot' />
-      <h6>
+      <h6 hidden>
         I'm&nbsp;<strong>Kyle&nbsp;Butler</strong>,
         <br />
         a&nbsp;full-stack&nbsp;web&nbsp;developer
@@ -13,6 +13,13 @@ export default function Welcome() {
         <strong>
           <a href='https://ezadtv.com/' target='_blank' rel="noreferrer" aria-label='Go to: ezadtv.com'>EZ-AD TV</a>
         </strong>
+      </h6>
+      <h6>
+        I'm&nbsp;<strong>Kyle&nbsp;Butler</strong>,
+        <br />
+        a&nbsp;full-stack&nbsp;web&nbsp;developer
+        <br />
+        working&nbsp;out&nbsp;of&nbsp;<strong>Los&nbsp;Angeles</strong>,&nbsp;CA
       </h6>
     </div>
   );
