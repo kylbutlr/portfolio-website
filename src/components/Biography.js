544460c5c946d4c3e07c48ec62ef7f40f751ba8d
Kyle Butler <kylbutlr@gmail.com>
2019-09-03 17:56:41 -0700

    minor change

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -7,18 +7,18 @@ export default function Biography() {
         I use <strong>JavaScript</strong> (<strong>React</strong>, <strong>Vue</strong>,{' '}
         <strong>Node</strong>, <strong>jQuery</strong>), <strong>PHP</strong>, <strong>SQL</strong>,{' '}
         <strong>CSS</strong>, and <strong>HTML</strong> to build anything from small business
-        websites to intuitive progressive&nbsp;web&nbsp;apps.
+        websites to intuitive progressive web&nbsp;applications.
       </p>
       <p>
         I have experience working with a variety of tools and concepts such as:{' '}
         <strong>RESTful APIs</strong>, <strong>PostgreSQL</strong>, <strong>MySQL</strong>,{' '}
         <strong>Express</strong>, <strong>Axios</strong>
         <span className='slant'>/</span>
-        <strong>Ajax</strong>, <strong>Redux</strong>, <strong>UX</strong>
+        <strong>Ajax</strong>, <strong>JSON</strong>, <strong>Redux</strong>, <strong>Jest</strong>, <strong>UX</strong>
         <span className='slant'>/</span>
-        <strong>UI Design</strong>, <strong>RWD</strong>, <strong>Bootstrap</strong>,{' '}
-        <strong>Bulma</strong>, <strong>Sass</strong>, <strong>Jest</strong>, <strong>JSON</strong>,{' '}
-        <strong>Babel</strong>, <strong>Webpack</strong>, <strong>SEO</strong>, <strong>NPM</strong>
+        <strong>UI&nbsp;Design</strong>, <strong>RWD</strong>, <strong>Bootstrap</strong>,{' '}
+        <strong>Bulma</strong>, <strong>Sass</strong>,{' '}
+        <strong>Babel</strong>, <strong>Webpack</strong>, <strong>WCAG</strong>, <strong>SEO</strong>, <strong>NPM</strong>
         ,&nbsp;<strong>Git</strong>,&nbsp;and&nbsp;more.
       </p>
       <p style={{ display: 'none' }}>
