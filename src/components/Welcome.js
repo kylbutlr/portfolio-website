74b4f57c635d499c5da20c26127e662fa65cd7d8
Kyle Butler <kylbutlr@gmail.com>
2020-08-10 11:30:53 -0700

    Minor updates

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,13 +7,12 @@ export default function Welcome() {
       <h6>
         I'm&nbsp;<strong><span class="large">Kyle&nbsp;Butler</span></strong>,&nbsp;a
         <br />
-        Front-End&nbsp;Web&nbsp;Developer
-        <br />
-        currently&nbsp;working&nbsp;with&nbsp;
+        Front-End&nbsp;Engineer&nbsp;at&nbsp;
         <strong>
           <a href='https://sdg.la/' target='_blank' rel='noreferrer' aria-label='Go to: SDG.la'>SDG</a>
         </strong>
         <span hidden>
+          currently&nbsp;working&nbsp;with&nbsp;
           working&nbsp;out&nbsp;of&nbsp;<strong>Los&nbsp;Angeles</strong>
         </span>
       </h6>
